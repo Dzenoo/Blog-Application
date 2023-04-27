80da32475580ac1c7bcc695608ff1307c1cc5012
Dzenoo <frontprogramer@gmail.com>
2023-04-27 22:54:28 +0200

    Adding all posts page

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import AllPosts from "../../components/posts/AllPosts";
+
+const DUMMY_POSTS = [
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs",
+  },
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs2",
+  },
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs3",
+  },
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs4",
+  },
+];
 
 const AllPostsPage = () => {
-  return <div>AllPostsPage</div>;
+  return (
+    <>
+      <AllPosts posts={DUMMY_POSTS} />
+    </>
+  );
 };
 
 export default AllPostsPage;
