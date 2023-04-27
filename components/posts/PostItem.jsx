bd83d4479d2fd87390a0786f8e6da9df180acd25
Dzenoo <frontprogramer@gmail.com>
2023-04-27 22:15:39 +0200

    Adding A post grid and post items

--- components/posts/PostItem.jsx
+++ components/posts/PostItem.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import classes from "./Posts-item.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-const PostItem = () => {
-  return <div>PostItem</div>;
+const PostItem = ({ title, image, date, excerpt, slug }) => {
+  const formatedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    day: "long",
+    day: "numeric",
+  });
+
+  const imagePath = `/images/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link>
+        <div className={classes.image}>
+          <Image src={imagePath} alt={title} width={200} height={200} />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <time>{formatedDate}</time>
+          <p>{excerpt}</p>
+        </div>
+      </Link>
+    </li>
+  );
 };
 
 export default PostItem;
