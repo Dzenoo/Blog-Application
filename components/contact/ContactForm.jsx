bb5fe8c460f7d4638e61e94e7250e816efd7e210
Dzenoo <frontprogramer@gmail.com>
2023-04-28 19:08:20 +0200

    Finishing contact page

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useRef } from "react";
 import classes from "./Contact-form.module.css";
 
 const ContactForm = () => {
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const messageRef = useRef();
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    const emailValue = emailRef.current.value;
+    const nameValue = nameRef.current.value;
+    const messageValue = messageRef.current.value;
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: emailValue,
+        name: nameValue,
+        message: messageValue,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
   return (
     <section className={classes.contact}>
       <h1>How can i help you?</h1>
-      <form className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input type="email" id="email" ref={emailRef} />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input type="text" id="name" ref={nameRef} />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea type="text" id="message" required />
+          <textarea type="text" id="message" ref={messageRef} />
         </div>
         <div className={classes.actions}>
-          <button>Send message</button>
+          <button type="submit">Send message</button>
         </div>
       </form>
     </section>
--- pages/api/contact.jsx
+++ pages/api/contact.jsx
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
@@ -10,8 +12,17 @@ async function handler(req, res) {
       !message ||
       message.trim() === ""
     ) {
-      res.status(422).json({ message: "Not valid data" });
+      res.status(422).json({ message: "Invalid input." });
       return;
+    }
+
+    let client;
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://Dzenoo:Hp3QVisDi1ej76vj@cluster0.8suhkcc.mongodb.net/blognextjs?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(422).json({ message: "Cannot connect to database" });
     }
 
     const newMessage = {
@@ -20,7 +31,23 @@ async function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
-    res.status(201).json({ message: "Successfully stored message" });
+    const db = client.db();
+
+    try {
+      const result = await db.collection("contact").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(201).json({ message: "Sended contact failed" });
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: "Successfully stored message", messageObj: newMessage });
   }
 }
+
+export default handler;
