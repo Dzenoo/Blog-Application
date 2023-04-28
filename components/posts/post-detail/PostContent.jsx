75b8e73d332fc086b4f3a59da1e724ab3ac40262
Dzenoo <frontprogramer@gmail.com>
2023-04-28 15:51:07 +0200

    Adding next image

--- components/posts/post-detail/PostContent.jsx
+++ components/posts/post-detail/PostContent.jsx
@@ -2,22 +2,50 @@ import React from "react";
 import classes from "./Post-content.module.css";
 import PostHeader from "./PostHeader";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import Image from "next/image";
 
-const DUMMY_POST = {
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  slug: "getting-started-with-nextjs",
-  content: "# This is a first post",
-};
+const PostContent = ({ post }) => {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
-const PostContent = (props) => {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const customerRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customerRenderers}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 };
--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import PostContent from "../../components/posts/post-detail/PostContent";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-const PostDetailPage = () => {
-  return <PostContent />;
+const PostDetailPage = (props) => {
+  return <PostContent post={props.post} />;
 };
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const post = getPostData(params.slug);
+
+  return {
+    props: {
+      post: post,
+    },
+    revalidate: 60,
+  };
+}
+
+export async function getStaticPaths() {
+  const data = getPostsFiles();
+  const pathParams = data.map((p) => p.replace(/\.md$/, ""));
+
+  return {
+    paths: pathParams.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
+}
 
 export default PostDetailPage;
